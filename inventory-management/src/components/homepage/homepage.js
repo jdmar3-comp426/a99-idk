3ab201c9335b5e3b00bd2f33a445b86b736691ed
shravans12 <shravans@live.unc.edu>
2021-11-25 12:23:54 -0500

    title.js

---
+++ inventory-management/src/components/homepage/homepage.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import Title from '/Title'
+
+class Homepage extends Component {
+    render() {
+        return <div>
+                <Title title={'Inventory Management'}/>
+        </div>
+    }
+}
+
+export default Homepage
+
