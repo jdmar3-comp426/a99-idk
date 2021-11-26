6a43eef0b8bd606c5c56b85eb273e6248ddf59c8
Yang Chen <yyangc88@gmail.com>
2021-11-26 10:21:32 -0500

    Changing homepage to functional

--- inventory-management/src/components/homepage/homepage.js
+++ inventory-management/src/components/homepage/homepage.js
@@ -1,12 +1,12 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Title from '/Title'
 
-class Homepage extends Component {
-    render() {
-        return <div>
-                <Title title={'Inventory Management'}/>
+function Homepage() {
+    return (
+        <div>
+            <Title title={'Inventory Management'} />
         </div>
-    }
+    );
 }
 
 export default Homepage
