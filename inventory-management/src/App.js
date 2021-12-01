58da15bdd561b53185d9ac6e41cb669af43f3c25
wutjut <rlesher@live.unc.edu>
2021-12-01 18:51:04 -0500

    basic front

---
+++ inventory-management/src/App.js
@@ -0,0 +1,176 @@
+import React, { useState, Fragment } from "react";
+import { nanoid } from "nanoid";
+import "./App.css";
+import data from "./mock-data.json";
+import ReadOnlyRow from "./components/ReadOnlyRow";
+import EditableRow from "./components/EditableRow";
+
+const App = () => {
+  const [contacts, setContacts] = useState(data);
+  const [addFormData, setAddFormData] = useState({
+    name: "",
+    amount: 0,
+    price: 0,
+  });
+
+  const [editFormData, setEditFormData] = useState({
+    name: "",
+    amount: 0,
+    price: 0,
+  });
+
+  const [editContactId, setEditContactId] = useState(null);
+
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...addFormData };
+    newFormData[fieldName] = fieldValue;
+
+    setAddFormData(newFormData);
+  };
+
+  const handleEditFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...editFormData };
+    newFormData[fieldName] = fieldValue;
+
+    setEditFormData(newFormData);
+  };
+
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newContact = {
+      id: nanoid(),
+      name: addFormData.name,
+      amount: addFormData.amount,
+      price: addFormData.price,
+    };
+
+    const newContacts = [...contacts, newContact];
+    setContacts(newContacts);
+  };
+
+  const handleEditFormSubmit = (event) => {
+    event.preventDefault();
+
+    const editedContact = {
+      id: editContactId,
+      name: editFormData.name,
+      amount: editFormData.amount,
+      price: editFormData.price,
+    };
+
+    const newContacts = [...contacts];
+
+    const index = contacts.findIndex((contact) => contact.id === editContactId);
+
+    newContacts[index] = editedContact;
+
+    setContacts(newContacts);
+    setEditContactId(null);
+  };
+
+  const handleEditClick = (event, contact) => {
+    event.preventDefault();
+    setEditContactId(contact.id);
+
+    const formValues = {
+      name: contact.name,
+      amount: contact.amount,
+      price: contact.price,
+    };
+
+    setEditFormData(formValues);
+  };
+
+  const handleCancelClick = () => {
+    setEditContactId(null);
+  };
+
+  const handleDeleteClick = (contactId) => {
+    const newContacts = [...contacts];
+
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+
+    newContacts.splice(index, 1);
+
+    setContacts(newContacts);
+  };
+
+  return (
+    <div className="app-container">
+      <form onSubmit={handleEditFormSubmit}>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Amount</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map((contact) => (
+              <Fragment>
+                {editContactId === contact.id ? (
+                  <EditableRow
+                    editFormData={editFormData}
+                    handleEditFormChange={handleEditFormChange}
+                    handleCancelClick={handleCancelClick}
+                  />
+                ) : (
+                  <ReadOnlyRow
+                    contact={contact}
+                    handleEditClick={handleEditClick}
+                    handleDeleteClick={handleDeleteClick}
+                  />
+                )}
+              </Fragment>
+            ))}
+          </tbody>
+        </table>
+      </form>
+
+      <h2>Add a Contact</h2>
+      <form onSubmit={handleAddFormSubmit}>
+        <input
+          type="text"
+          name="name"
+          required="required"
+          placeholder="Enter a name..."
+          onChange={handleAddFormChange}
+        />
+        <input
+          type="number"
+          name="amount"
+          required="required"
+          placeholder="Enter an amount..."
+          onChange={handleAddFormChange}
+        />
+        <span class="currencyinput">$
+          <input
+            type="number"
+            class = "currency"
+            step="0.01"
+            name="price"
+            required="required"
+            placeholder="Enter a price..."
+            onChange={handleAddFormChange}
+          />
+        </span>
+        
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
---
+++ inventory-management/src/components/EditableRow.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const EditableRow = ({
+  editFormData,
+  handleEditFormChange,
+  handleCancelClick,
+}) => {
+  return (
+    <tr>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter a name..."
+          name="fullName"
+          value={editFormData.name}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="number"
+          required="required"
+          placeholder="Enter an amount..."
+          name="amount"
+          value={editFormData.amount}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+      <span class="currencyinput">$
+          <input
+            type="number"
+            step="0.01"
+            class = "currency"
+            required="required"
+            placeholder="Enter a price..."
+            name="price"
+            value={editFormData.price}
+            onChange={handleEditFormChange}
+          ></input>
+        </span>
+        
+      </td>
+      <td>
+        <button type="submit">Save</button>
+        <button type="button" onClick={handleCancelClick}>
+          Cancel
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default EditableRow;
